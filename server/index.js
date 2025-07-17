c9b63695c8e513ca38a2fcd7b1ce007c2ee9e660
Ahmed Farooq <160997141+ahmed-workstreamautomation@users.noreply.github.com>
2025-07-17 11:32:33 +0500

    Initiated server folder

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parses JSON bodies
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
