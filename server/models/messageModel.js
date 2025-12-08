59ace46f8f334667c8fc6e18303211031fe4d8a9
Ahmed Farooq <160997141+ahmed-workstreamautomation@users.noreply.github.com>
2025-12-08 16:51:06 +0500

    some modifications done

---
+++ server/models/messageModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    room: { type: String, required: true },
+    sender: { type: String, required: true },
+    text: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
